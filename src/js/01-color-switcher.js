9a03dac4f470cfdc2ab4cfe79bc001e7ef30bbf8
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-24 13:39:46 -0600

    dz09 (01)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+// console.log(stopBtn);
+
+startBtn.addEventListener('click', handelStart);
+function handelStart(event) {
+  if (event) {
+    const colorChange = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 600);
+
+    stopBtn.addEventListener('click', handelStop);
+    function handelStop(event) {
+      if (event) {
+        clearInterval(colorChange);
+      }
+    }
+  }
+}
